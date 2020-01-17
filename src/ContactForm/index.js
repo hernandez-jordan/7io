e7f58b447d60ac9006583a21f3ad55d065d65bdb
Hernan Antonio Martin Castillo <hello@clevercode.nl>
2020-01-17 23:32:07 +0100

    fix: test new mail server

--- src/ContactForm/index.js
+++ src/ContactForm/index.js
@@ -162,7 +162,7 @@ const ContactForm = props => {
       return;
     }
     // // eslint-disable-next-line
-    await axios.post('https://7io-mail.wevisualizeit.now.sh/api/sendMail', {
+    await axios.post('https://zeit-test.wevisualizeit.now.sh/api/sendMail', {
       firstName, lastName, email, phoneNumber, companyName, message,
     }).then((response) => {
       console.log('response:', response);
