14af5db60d31887e4c6f061597bffaf8f17ac08a
Jordan Hernandez <hello@wevisualize.it>
2020-01-30 21:25:13 +0100

    Hotfix: 404

--- src/Pages/404.js
+++ src/Pages/404.js
@@ -1,11 +1,9 @@
 import React from "react";
 import PageNotFoundBanner from "../HeroBanner/404";
-import Nav from "../Nav/index";
 
 const PageNotFound = () => {
   return (
     <>
-      <Nav />
       <PageNotFoundBanner />
     </>
   );
