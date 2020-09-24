52f765d7c2fe1e42d660c39c0aa7057d659388b0
Jordan Hernandez <hello@wevisualize.it>
2020-09-24 15:12:30 +0200

    Fix: optimise code in footer component

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,90 +1,194 @@
 import React from "react";
-import {
-  makeStyles,
-  Typography,
-  Container,
-  Grid
-} from "@material-ui/core";
+import { makeStyles, Typography, Container, Grid } from "@material-ui/core";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   outer: {
     backgroundColor: "#331D48",
     paddingTop: 50,
     paddingBottom: 50,
     height: "auto",
-    width: "100%"
+    width: "100%",
   },
   container: {
-    margin: "50px auto"
+    margin: "50px auto",
   },
   componentTitle: {
     margin: "50px auto",
     textAlign: "center",
     fontWeight: "bold",
-    color: "#331D48"
+    color: "#331D48",
   },
-  grid : {
-    display: 'flex',
+  grid: {
+    display: "flex",
     [theme.breakpoints.down("xs")]: {
-      textAlign: 'center',
-      display: 'flex',
-      flexDirection: 'column',
-      },
-  },  
-  gridItem : {
-    display: 'flex',
-    flexDirection: 'column',
-    margin: '20px 0',
-    paddingRight: '20px'
-  },  
+      textAlign: "center",
+      display: "flex",
+      flexDirection: "column",
+    },
+  },
+  gridItem: {
+    display: "flex",
+    flexDirection: "column",
+    margin: "20px 0",
+    paddingRight: "20px",
+  },
 }));
 
+const grids = [
+  {
+    xs: 12,
+    sm: 6,
+    md: 6,
 
+    contents: [
+      {
+        variant: "h3",
+        component:"h1",
+        texts: [{ text: `7io` }],
+        style: [
+          {
+            display: "inline-block",
+            color: "white",
+            paddingBottom: null,
+          },
+        ],
+      },
+      {
+        variant: null,
+        component: "h3",
+        texts: [{ text: `Combining the Best` }],
+        style: [
+          {
+            display: "inline-block",
+            color: "white",
+            paddingBottom: "20px",
+          },
+        ],
+      },
+      {
+        variant: null,
+        component: "subtitle1",
+        texts: [{ text: `Copyright © 2019 7io, Powered by We Visualize It` }],
+        style: [
+          {
+            display: "inline-block",
+            color: "white",
+            paddingBottom: null,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    xs: 12,
+    sm: 3,
+    md: 3,
+
+    contents: [
+      {
+        variant: "h5",
+        component: "h2",
+        texts: [{ text: `Company` }],
+        style: [
+          {
+            display: "inline-block",
+            color: "white",
+            paddingBottom: "20px",
+          },
+        ],
+      },
+      {
+        variant: "body1",
+        component: "p",
+        texts: [
+          { text: `KVK: 75374390` },
+          { text: `BTW-Number: NL860257411B01` },
+          { text: `Phone: +31106690867` },
+        ],
+        style: [
+          {
+            display: "inline-block",
+            color: "white",
+            paddingBottom: null,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    xs: 12,
+    sm: 3,
+    md: 3,
+
+    contents: [
+      {
+        variant: "h5",
+        component: "h2",
+        texts: [{ text: `Contact us` }],
+        style: [
+          {
+            display: "inline-block",
+            color: "white",
+            paddingBottom: "20px",
+          },
+        ],
+      },
+      {
+        variant: "body1",
+        component: "p",
+        texts: [
+          { text: `E-mail: contact@7io.net` },
+          { text: `Address: Wijnhaven 3 D` },
+          { text: `3011WG Rotterdam` },
+        ],
+        style: [
+          {
+            display: "inline-block",
+            color: "white",
+            paddingBottom: null,
+          },
+        ],
+      },
+    ],
+  },
+];
 
 const Footer = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return(
-      <div className={classes.outer}>
-        <Container className={classes.container}>
-              <Grid className={classes.grid}>
-                  <Grid item className={classes.gridItem} xs={12} sm={6} md={6}>
-                    <Typography variant="h3" component="h3" style={{display: 'inline-block', color: 'white'}}>
-                        7io <br/>
-                    </Typography>
-                    <Typography variant="subtitle1" component="h3" style={{display: 'inline-block', color: 'white', paddingBottom:'20px'}}>
-                        Combining the Best 
-                    </Typography>
-                    <Typography variant="body2" component="h3" style={{display: 'inline-block', color: 'white'}}>
-                      Copyright © 2019 7io, Powered by <em>We Visualize It</em>
-                    </Typography>
-                  </Grid> 
-                  
-                  <Grid item className={classes.gridItem}  xs={12} sm={3} md={3}>
-                    <Typography variant="h5" component="h3" style={{display: 'inline-block', color: 'white', paddingBottom: '20px'}}>
-                        Company <br/>
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p" style={{display: 'inline-block', color: 'white'}}>
-                        KVK: 75374390 <br/>
-                        BTW-Number: NL860257411B01 <br/>
-                        Phone: +31106690867  <br/>
-                    </Typography>
-                  </Grid> 
-                  <Grid item className={classes.gridItem}  xs={12} sm={3} md={3}>
-                    <Typography variant="h5" component="h3" style={{display: 'inline-block', color: 'white', paddingBottom: '20px'}}>
-                        Contact Us <br/>
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p" style={{display: 'inline-block', color: 'white'}}>
-                        E-mail: contact@7io.net <br/>
-                        Address: Wijnhaven 3 D, <br/>
-                        3011WG Rotterdam  <br/>
-                    </Typography>
-                  </Grid> 
-              </Grid>
-          </Container>
-      </div>
-       
-    );
+  return (
+    <div className={classes.outer}>
+      <Container className={classes.container}>
+        <Grid className={classes.grid}>
+          {grids.map((grid, key) => (
+            <Grid
+              key={key}
+              item
+              className={classes.gridItem}
+              xs={grid.xs}
+              sm={grid.sm}
+              md={grid.m}
+            >
+              {grid.contents.map((content, keyI) => (
+                <Typography
+                  key={keyI}
+                  variant={content.variant}
+                  component={content.component}
+                  style={content.style[0]}
+                >
+                  {content.texts.map((text) => (
+                    <>
+                      {text.text} <br />
+                    </>
+                  ))}
+                </Typography>
+              ))}
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </div>
+  );
 };
 
 export default Footer;
